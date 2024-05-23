5178e6ba3f49e8e132c450e81323b26890db30c7
Ashray <ashraydey2011@gmail.com>
2024-05-23 21:40:43 +0530

    Fixed firebase config

--- src/firebaseinnit.js
+++ src/firebaseinnit.js
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCwV-qbf_pOuwt1mxQm2j18OvtezmfUfh4",
-  authDomain: "buybusy-36c6a.firebaseapp.com",
-  projectId: "buybusy-36c6a",
-  storageBucket: "buybusy-36c6a.appspot.com",
-  messagingSenderId: "410120097584",
-  appId: "1:410120097584:web:38775be537f3d6497d42f5"
+  apiKey: 'AIzaSyAXhKyD7iMq-pyQd0ot6eOf44JZaDDOOaQ',
+  authDomain: 'buybusy-b3e97.firebaseapp.com',
+  projectId: 'buybusy-b3e97',
+  storageBucket: 'buybusy-b3e97.appspot.com',
+  messagingSenderId: '380611081244',
+  appId: '1:380611081244:web:4ba5b9bb83e23616c71bd9'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db= getFirestore(app);
+export const db = getFirestore(app);
